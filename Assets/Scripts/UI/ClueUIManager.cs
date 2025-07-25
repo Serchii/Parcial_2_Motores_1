1174454cd5035216f245792a578c66af5a38d180
SsGiann <giansessa4444@gmail.com>
2025-07-25 15:15:35 -0300

    detalles en las pistas
    detalles en las pistas

--- Assets/Scripts/UI/ClueUIManager.cs
+++ Assets/Scripts/UI/ClueUIManager.cs
@@ -1,35 +1,50 @@
-using UnityEngine;
 using TMPro;
+using UnityEngine;
 
 public class ClueUIManager : MonoBehaviour
 {
-    public static ClueUIManager Instance;
+    public static ClueUIManager Instance { get; private set; }
 
-    public TextMeshProUGUI clueText;
-    private int current = 0;
+    [SerializeField] private TextMeshProUGUI clueText;
+
     private int max = 0;
+    private int current = 0;
 
     private void Awake()
     {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
         Instance = this;
+    }
+
+    private void Start()
+    {
+        if (LevelManager.Instance != null && LevelManager.Instance.currentLevelData != null)
+        {
+            SetClueGoal(LevelManager.Instance.currentLevelData.requiredClues);
+        }
     }
 
     public void SetClueGoal(int maxClues)
     {
         max = maxClues;
         current = 0;
+        Debug.Log($"[ClueUIManager] SetClueGoal llamado: {current}/{max}");
         UpdateUI();
     }
 
     public void AddClue()
     {
         current++;
+        Debug.Log($"[ClueUIManager] AddClue: {current}/{max}");
         UpdateUI();
     }
 
     private void UpdateUI()
     {
-        if (clueText != null)
-            clueText.text = $"Pistas: {current} / {max}";
+        clueText.text = $"{current} / {max}";
     }
 }
